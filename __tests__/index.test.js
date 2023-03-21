09fd1cbdd961d0ed3193f8ae4b85260d1c609b8b
Carnivorouscake <110086166+Carnivorouscake@users.noreply.github.com>
2023-03-21 20:18:05 +0300

    fix: test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,11 +1,19 @@
 // import { expect, test } from "@jest/globals";
 
-// import getDiff from '../src/utils.js';
+import getDiff from '../src/utils.js';
 
-const filePath1 = '__fixtures__/file1.json';
-const filePath2 = '__fixtures__/file2.json';
-test('gendiff', () => {
-    const str = '- follow: false,host: hexlet.io,- proxy: 123.234.53.22,- timeout: 50,+ timeout: 20,+ verbose: true';
-    expect(getDiff(filePath1, filePath2)).toBe(str.replace(',', '\n'));
+//const getdiff = getdiff();
+const filePath1 = '__tests__/__fixtures__/file1.json';
+const filePath2 = '__tests__/__fixtures__/file2.json';
+
+// const str = '- follow: false,host: hexlet.io,- proxy: 123.234.53.22,- timeout: 50,+ timeout: 20,+ verbose: true';
+
+
+// if(getdiff(filePath1, filePath2) !== str.replace(',', '\n')){
+//     throw new Error('error!')
+// }
+ test('gendiff', () => {
+     const str = '- follow: false\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ timeout: 20\n+ verbose: true';
+     expect(getDiff(filePath1, filePath2)).toBe(str);
     //expect(getDiff().toBe(''))
 })
